7b1a4ebc4173ff5c7a4a025b7fb52f77a4e7390f
alelopdel <alelopdel@alu.edu.gva.es>
2024-01-19 10:03:16 +0100

    1

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,12 +1,51 @@
 <script xmlns="http://www.w3.org/1999/html">
+import emailjs from 'emailjs-com';
+
 export default {
-  data(){
-    return{
+  data() {
+    return {
       email: "",
-      subject:"",
-      name:"",
-      telf:"",
-      body:""
+      subject: "",
+      name: "",
+      telf: "",
+      body: "",
+      snackbar: false,
+      text:""
+    }
+  }, methods: {
+    enviarCorreo() {
+      const templateParams = {
+        to_email: 'anchordepartment@gmail.com',
+        subject: this.subject,
+        message: 'Hola, soy ' + this.name + '\n' +
+            'Telf: ' + this.telf + '\n' +
+            'Email: ' + this.email + '\n' +
+            'Causa del contacto: \n' + this.body
+      };
+
+      if (this.name==="" || this.subject==="" || this.body==="" || this.telf==="" || this.email==="") {
+        this.text="Campos vacios";
+      }else{
+        emailjs.send(
+            'service_gmailAnchor',
+            'template_Anchor',
+            templateParams,
+            'DjMfAW2th2o_Ic2Il'
+        )
+            .then(response => {
+              this.body = "";
+              this.name = "", this.telf = "", this.email = "";
+              this.subject = "";
+              this.text = "Correo enviado"
+
+            })
+            .catch(error => {
+              this.text = "Fallo al enviar correo"
+              console.error('Error al enviar el correo', error);
+            });
+      }
+
+      this.snackbar=true;
     }
   }
 }
@@ -43,19 +82,30 @@ export default {
     -->
     <v-container style="max-width: none ; margin-top: 50px;width: 60%" class="align-center">
       <form style="width: 100%;">
-        <label for="Name" style="font-size: 20px" v-model="this.name">Nombre y Apellidos</label>
-        <v-text-field name="Name"></v-text-field>
-        <label for="Email" style="font-size: 20px" v-model="this.email">Correo</label>
-        <v-text-field name="Email"></v-text-field>
-        <label for="Telf" style="font-size: 20px" v-model="this.telf">Teléfono</label>
-        <v-text-field name="Telf"></v-text-field>
-        <label for="Subject" style="font-size: 20px" v-model="this.subject">Asunto</label>
-        <v-text-field name="Subject"></v-text-field>
-        <label for="Reason" style="font-size: 20px" v-model="this.body">Escribe aquí tu consulta para ayudarte</label>
-        <v-textarea name="Reason"></v-textarea>
+        <label for="Name" style="font-size: 20px">Nombre y Apellidos</label>
+        <v-text-field name="Name" v-model="this.name"></v-text-field>
+        <label for="Email" style="font-size: 20px">Correo</label>
+        <v-text-field name="Email" v-model="this.email"></v-text-field>
+        <label for="Telf" style="font-size: 20px">Teléfono</label>
+        <v-text-field name="Telf" v-model="this.telf"></v-text-field>
+        <label for="Subject" style="font-size: 20px">Asunto</label>
+        <v-text-field name="Subject" v-model="this.subject"></v-text-field>
+        <label for="Reason" style="font-size: 20px">Escribe aquí tu consulta para ayudarte</label>
+        <v-textarea name="Reason" v-model="this.body"></v-textarea>
         <div class="align-center">
-          <!--<v-btn class="bg-blue" style="font-size: 20px" :href="">Enviar</v-btn>-->
-          {{this.body}}
+          <v-btn class="bg-blue" style="font-size: 20px" @click="enviarCorreo">Enviar</v-btn>
+          <v-snackbar multi-line v-model="snackbar">
+            {{ this.text }}
+            <template v-slot:actions>
+              <v-btn
+                  color="white"
+                  variant="text"
+                  @click="snackbar = false"
+              >
+                Close
+              </v-btn>
+            </template>
+          </v-snackbar>
         </div>
       </form>
     </v-container>
