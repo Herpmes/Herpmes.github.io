c2133b74745c8d6389138bdefcc2d7682092f2f3
aarricgra <aaricgra@alu.edu.gva.es>
2024-02-01 14:59:09 +0100

    Primer coso

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script>
 import {RouterLink, RouterView} from 'vue-router'
+import AppBarDesktop from 'src/components/AppBarDesktop.vue'
   export default {
     data() {
-      return {selectedTopTab: 0, drawer: false}
+      return {selectedTopTab: 0, drawerProductos: false,drawerMobile:false}
     }, methods: {
       changeSelected(newSelected) {
         this.selectedTopTab = newSelected
@@ -16,33 +17,53 @@ import {RouterLink, RouterView} from 'vue-router'
     ******************
     Menu superior
     ******************
+    v-if="this.$vuetify.display.name!=='xs'"
     -->
-    <v-app-bar fixed style="height: 60px"><img src="/src/Img/Logos/LogoHerpmes.png" alt="" class="logoH">
+    <AppBarDesktop />
+    <v-app-bar fixed>
+
+
+      <img src="/src/Img/Logos/LogoHerpmes.png" alt="" class="logoH">
+
+
+
       <!--Home-->
       <RouterLink to="/">
-        <v-btn class="font-weight-bold" style="color:#24afff" :ripple="false" @click="changeSelected(0)"
+        <v-btn class="font-weight-bold" color="#24afff" :ripple="false" @click="changeSelected(0)"
                v-if="this.selectedTopTab===0">Inicio
         </v-btn>
         <v-btn class="font-weight-bold" :ripple="false" @click="changeSelected(0)" v-else>Inicio</v-btn>
       </RouterLink>
 
+
+
+
       <!--About-->
       <RouterLink to="/about">
-        <v-btn class="font-weight-bold" style="color:#24afff" :ripple="false" @click="changeSelected(1)"
+        <v-btn class="font-weight-bold" color="#24afff" :ripple="false" @click="changeSelected(1)"
                v-if="this.selectedTopTab===1">Quienes Somos
         </v-btn>
         <v-btn class="font-weight-bold" :ripple="false" @click="changeSelected(1)" v-else>Quienes Somos</v-btn>
       </RouterLink>
       <v-btn class="font-weight-bold" :ripple="false" @mouseenter="drawer = !drawer">Productos</v-btn>
 
+
+
       <v-spacer></v-spacer>
+
+
+
+
       <!--Contact Us-->
       <RouterLink to="/contact">
-        <v-btn class="hover-anchor font-weight-bold" style="color:#24afff" :ripple="false" @click="changeSelected(5)"
+        <v-btn class="hover-anchor font-weight-bold" color="#24afff" :ripple="false" @click="changeSelected(5)"
                v-if="this.selectedTopTab===5">CONTACTANOS
         </v-btn>
         <v-btn class="hover-anchor font-weight-bold" :ripple="false" @click="changeSelected(5)" v-else>CONTACTANOS</v-btn>
       </RouterLink>
+
+
+
 
       <!--Login-->
       <v-btn class="font-weight-bold" :ripple="false">LOGIN</v-btn>
@@ -53,14 +74,14 @@ import {RouterLink, RouterView} from 'vue-router'
     ******************
     -->
       <v-navigation-drawer
-        v-model="drawer"
+        v-model="drawerProductos"
         location="top"
         temporary
-        style="margin-top: -5px;"
-        @mouseleave="drawer = !drawer">
-        <v-row style="margin-top: 20px;padding: 40px">
+        margin-top="-5px"
+        @mouseleave="drawerProductos = !drawerProductos" >
+        <v-row class="pa-10">
           <v-col cols="3" >
-            <h4 style="color: #00b0ff;font-weight: bold">GRANDES EMPRESAS</h4>
+            <h4 class="tituloProductos">GRANDES EMPRESAS</h4>
               <v-row style="margin-top: 5px">
                 <v-col cols="2">
                   foto
@@ -71,13 +92,13 @@ import {RouterLink, RouterView} from 'vue-router'
               </v-row>
           </v-col>
           <v-col cols="3">
-            <h4 style="color: #00b0ff;font-weight: bold">PYMES</h4>
+            <h4 class="tituloProductos">PYMES</h4>
           </v-col>
           <v-col cols="3">
-            <h4 style="color: #00b0ff;font-weight: bold">AUTÓNOMOS</h4>
+            <h4 class="tituloProductos">AUTÓNOMOS</h4>
           </v-col>
           <v-col cols="3" style="color: #00b0ff;font-weight: bold">
-            <h4 style="color: #00b0ff;font-weight: bold">HERRAMIENTAS DE DESARROLLO</h4>
+            <h4 class="tituloProductos">HERRAMIENTAS DE DESARROLLO</h4>
           </v-col>
         </v-row>
       </v-navigation-drawer>
@@ -92,9 +113,9 @@ import {RouterLink, RouterView} from 'vue-router'
         <v-row>
           <v-col cols="4"></v-col>
           <v-col cols="4">
-            <v-btn icon="mdi-instagram" class="btn-margin" href="https://www.instagram.com/herpmes.enterprise/"></v-btn>
-            <v-btn icon="mdi-twitter" class="btn-margin" href="https://twitter.com/H3RPmes"></v-btn>
-            <v-btn icon="mdi-email" class="btn-margin" href="mailto:herpmes.entrerprise@gmail.com"></v-btn>
+            <v-btn icon="mdi-instagram" class="me-2" href="https://www.instagram.com/herpmes.enterprise/"></v-btn>
+            <v-btn icon="mdi-twitter" class="me-2" href="https://twitter.com/H3RPmes"></v-btn>
+            <v-btn icon="mdi-email" class="me-2" href="mailto:herpmes.entrerprise@gmail.com"></v-btn>
           </v-col>
           <v-col cols="4"></v-col>
         </v-row>
@@ -102,37 +123,17 @@ import {RouterLink, RouterView} from 'vue-router'
     </footer>
   </v-app>
 </template>
-<style>.selected-home {
-  color: #24afff;
+<style>
+
+.tituloProductos{
+  color: #00b0ff;font-weight: bold
 }
 
-.selected-about {
-  color: #24afff;
-}
 
 .hover-anchor:hover {
   color: #24afff;
 }
 
-.selected-anchor {
-  color: #24afff;
-}
-
-.hover-nuixo:hover {
-  color: #ff8787;
-}
-
-.selected-nuixo {
-  color: #ff8787;
-}
-
-.hover-codec:hover {
-  color: #9489f5;
-}
-
-.selected-codec {
-  color: #9489f5;
-}
 
 .custom-footer {
   background-color: #333; /* Color de fondo del footer */
@@ -143,11 +144,4 @@ import {RouterLink, RouterView} from 'vue-router'
   margin-top: 50px; /* Empuja el footer hacia abajo al final del contenedor */
 }
 
-.btn-margin {
-  margin-right: 5px;
-  margin-left: 5px;
-}
-
-.btn-margin:hover {
-  color: #146CC4;
-}</style>
+</style>
---
+++ src/components/AppBarDesktop.vue
@@ -0,0 +1,69 @@
+<script>
+export default {
+  data() {
+    return {selectedTopTab: 0}
+  }, methods: {
+    changeSelected(newSelected) {
+      this.selectedTopTab = newSelected
+    }
+  }
+}
+</script>
+
+<template>
+  <v-app-bar fixed>
+
+
+    <img src="/src/Img/Logos/LogoHerpmes.png" alt="" class="logoH">
+
+
+
+    <!--Home-->
+    <RouterLink to="/">
+      <v-btn class="font-weight-bold" color="#24afff" :ripple="false" @click="changeSelected(0)"
+             v-if="this.selectedTopTab===0">Inicio
+      </v-btn>
+      <v-btn class="font-weight-bold" :ripple="false" @click="changeSelected(0)" v-else>Inicio</v-btn>
+    </RouterLink>
+
+
+
+
+    <!--About-->
+    <RouterLink to="/about">
+      <v-btn class="font-weight-bold" color="#24afff" :ripple="false" @click="changeSelected(1)"
+             v-if="this.selectedTopTab===1">Quienes Somos
+      </v-btn>
+      <v-btn class="font-weight-bold" :ripple="false" @click="changeSelected(1)" v-else>Quienes Somos</v-btn>
+    </RouterLink>
+
+
+    <!--Productos-->
+    <v-btn class="font-weight-bold" :ripple="false" @mouseenter="drawerProductos = !drawerProductos">Productos</v-btn>
+
+
+
+    <v-spacer></v-spacer>
+
+
+
+
+    <!--Contact Us-->
+    <RouterLink to="/contact">
+      <v-btn class="hover-anchor font-weight-bold" color="#24afff" :ripple="false" @click="changeSelected(5)"
+             v-if="this.selectedTopTab===5">CONTACTANOS
+      </v-btn>
+      <v-btn class="hover-anchor font-weight-bold" :ripple="false" @click="changeSelected(5)" v-else>CONTACTANOS</v-btn>
+    </RouterLink>
+
+
+
+
+    <!--Login-->
+    <v-btn class="font-weight-bold" :ripple="false">LOGIN</v-btn>
+  </v-app-bar>
+</template>
+
+<style scoped>
+
+</style>
