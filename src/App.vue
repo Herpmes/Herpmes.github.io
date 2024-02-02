f8bd5a255d1a53ecb6cf3fac75cde3125434907b
aarricgra <aaricgra@alu.edu.gva.es>
2024-02-02 08:51:32 +0100

    Menu Chiquito

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
-<script>
+<script setup>
 import {RouterLink, RouterView} from 'vue-router'
-import AppBarDesktop from 'src/components/AppBarDesktop.vue'
-  export default {
-    data() {
-      return {selectedTopTab: 0, drawerProductos: false,drawerMobile:false}
-    }, methods: {
-      changeSelected(newSelected) {
-        this.selectedTopTab = newSelected
-      }
-    }
-  }
+import AppBarDesktop from './components/AppBarDesktop.vue'
+import AppBarMobile from './components/AppBarMobile.vue'
+
+let drawerProductos = false;
+let drawerMobile = false;
+function changeSelected(newSelected) {
+  this.selectedTopTab = newSelected
+}
 </script>
 <template>
   <v-app>
@@ -19,55 +17,8 @@ import AppBarDesktop from 'src/components/AppBarDesktop.vue'
     ******************
     v-if="this.$vuetify.display.name!=='xs'"
     -->
-    <AppBarDesktop />
-    <v-app-bar fixed>
-
-
-      <img src="/src/Img/Logos/LogoHerpmes.png" alt="" class="logoH">
-
-
-
-      <!--Home-->
-      <RouterLink to="/">
-        <v-btn class="font-weight-bold" color="#24afff" :ripple="false" @click="changeSelected(0)"
-               v-if="this.selectedTopTab===0">Inicio
-        </v-btn>
-        <v-btn class="font-weight-bold" :ripple="false" @click="changeSelected(0)" v-else>Inicio</v-btn>
-      </RouterLink>
-
-
-
-
-      <!--About-->
-      <RouterLink to="/about">
-        <v-btn class="font-weight-bold" color="#24afff" :ripple="false" @click="changeSelected(1)"
-               v-if="this.selectedTopTab===1">Quienes Somos
-        </v-btn>
-        <v-btn class="font-weight-bold" :ripple="false" @click="changeSelected(1)" v-else>Quienes Somos</v-btn>
-      </RouterLink>
-      <v-btn class="font-weight-bold" :ripple="false" @mouseenter="drawer = !drawer">Productos</v-btn>
-
-
-
-      <v-spacer></v-spacer>
-
-
-
-
-      <!--Contact Us-->
-      <RouterLink to="/contact">
-        <v-btn class="hover-anchor font-weight-bold" color="#24afff" :ripple="false" @click="changeSelected(5)"
-               v-if="this.selectedTopTab===5">CONTACTANOS
-        </v-btn>
-        <v-btn class="hover-anchor font-weight-bold" :ripple="false" @click="changeSelected(5)" v-else>CONTACTANOS</v-btn>
-      </RouterLink>
-
-
-
-
-      <!--Login-->
-      <v-btn class="font-weight-bold" :ripple="false">LOGIN</v-btn>
-    </v-app-bar>
+    <AppBarDesktop v-if="this.$vuetify.display.name!=='xs'"/>
+    <AppBarMobile v-else/>
     <!--
     ******************
     View que cambia
---
+++ src/components/AppBarMobile.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {selectedTopTab: 0}
+  }, methods: {
+    changeSelected(newSelected) {
+      this.selectedTopTab = newSelected
+    }
+  }
+}
+</script>
+
+<template>
+  <v-app-bar fixed>
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+
+    <img src="/src/Img/Logos/LogoHerpmes.png" alt="" class="logoH">
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+
+<style scoped>
+
+</style>
