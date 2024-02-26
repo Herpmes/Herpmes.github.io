bbd5df9850febe11ca1988873e228f6c2de89a7b
angalemen <angalmen@alu.edu.gva.es>
2024-02-26 09:04:39 +0100

    aaaa

--- src/views/Departments/Nuix/NuixoView.vue
+++ src/views/Departments/Nuix/NuixoView.vue
@@ -3,7 +3,21 @@
 </script>
 
 <template>
+  <p>     </p>
+  <p>     </p>
+  <p>     </p>
+  <p>     </p>
+  <p>     </p>
+  <p>     </p>
+  <p>     </p>
+  <p>     </p>
+  <p>     </p>
+
 <h1>weeeeeeeeee</h1>
+  <h1>weeeeeeeeee2</h1>
+  <img href="src/Img/img Nuixo/nuixologo.jpg" alt="Nuixo Logo">
+
+
 </template>
 
 <style scoped>
